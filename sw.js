4deb09686dd0928f3b0dc943a693bec90ce39b9f
Nitish <nitishrchaubey9926@gmail.com>
2026-08-12 23:42:28 +0530

    fix(sw): prefer network for JavaScript assets

--- sw.js
+++ sw.js
@@ -38,8 +38,24 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-// Fetch Assets (Cache First Strategy)
 self.addEventListener('fetch', (event) => {
+    if (event.request.destination === 'script') {
+        event.respondWith(
+            fetch(event.request)
+                .then((response) => {
+                    const responseClone = response.clone();
+
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseClone);
+                    });
+
+                    return response;
+                })
+                .catch(() => caches.match(event.request))
+        );
+        return;
+    }
+
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
